bd4aff5a49efca9fff496d25e03583efa20eff8c
Sarah Prando <sarahprando40@gmail.com>
2025-03-13 07:49:34 -0300

    project completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import plugin from 'tailwindcss/plugin';
+
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
@@ -9,7 +11,36 @@ module.exports = {
         sm: '1rem',
       },
     },
+
     extend: {
+      fontFamily: {
+        sans: ['DM Sans', 'system-ui', 'sans-serif'],
+        serif: ['DM Serif Text', 'Georgia', 'serif'],
+      },
+      keyframes: {
+        slideIn: {
+          '0%': {
+            opacity: 0,
+            transform: 'translateX(-20px)',
+          },
+          '100%': {
+            opacity: 1,
+            transform: 'translateX(0)',
+          },
+        },
+        fadeIn: {
+          '0%': {
+            opacity: 0,
+          },
+          '100%': {
+            opacity: 1,
+          },
+        },
+      },
+      animation: {
+        ['slide-in']: 'slideIn .4s ease-in-out forwards',
+        ['fade-in']: 'fadeIn .4s ease-in-out forwards',
+      },
       colors: {
         verde: {
           200: '#ACEF75',
@@ -23,6 +54,19 @@ module.exports = {
       },
     },
   },
-  plugins: [],
-}
+  plugins: [
+    plugin(({ addUtilities }) => {
+      function animationDelay() {
+        const delays = {};
+        for (let i = 0; i <= 12; i++) {
+          delays[`.animate-${i}`] = {
+            'animation-delay': `${i * 100}ms`,
+          };
+        }
+        return delays;
+      }
 
+      addUtilities(animationDelay());
+    }),
+  ],
+};
